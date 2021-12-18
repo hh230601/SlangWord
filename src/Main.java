76ec18eb2e465fdbee452b7bf3f09f9faa968be7
hh230601 <huyhoang23062001@gmail.com>
2021-12-18 23:34:22 +0700

    Update function

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,13 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
 
+
+
+    // Read Slang Word from file
     public static void ReadSlang(String filename, ArrayList<Slang> list) throws IOException{
         list.clear();
         BufferedReader reader = new BufferedReader(new FileReader(filename));
@@ -19,11 +23,56 @@ public class Main {
         reader.close();
     }
 
+    // Search slang word
+    public static String SearchSlangWord(ArrayList<Slang> list, String slangword) {
+        for (Slang i : list) {
+            if (i.getSlang().equalsIgnoreCase(slangword))
+                return i.getMeaning();
+        }
+        return null;
+    }
+
+    // Search meaning
+    public static ArrayList<String> SearchMeaning(ArrayList<Slang> list, String meaning){
+       ArrayList<String> slang = new ArrayList<String>();
+        for (Slang i : list) {
+            if (i.getMeaning().contains(meaning))
+                slang.add(i.getSlang());
+        }
+        return slang;
+    }
+
+    // Edit slang word
+    public static void EditSlang(ArrayList<Slang> list, Slang edit){
+        for (Slang i : list) {
+            if (i.getSlang().contains(edit.getSlang())){
+                i.setMeaning(edit.getMeaning());
+                break;
+            }
+        }
+    }
+
+    //
     public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
         ArrayList<Slang> list  = new ArrayList<Slang>();
         ReadSlang("D:\\slang.txt",list);
-        for(Slang i : list){
-            System.out.println(i.getSlang()  + " " + i.getMeaning());
+        //System.out.print("Enter slang word: ");
+        //String slangword = scan.nextLine();
+        //String result = SearchSlangWord(list,slangword);
+        //if(result == null)
+        //    System.out.println("Slang word doesn't exist !");
+        //else
+        //    System.out.println("Meaning of "+ slangword + " is " + result);
+        System.out.print("Enter meaning: ");
+        String meaning = scan.nextLine();
+        ArrayList<String> slanglist = SearchMeaning(list,meaning);
+        if(slanglist.size() == 0)
+            System.out.println("Meaning doesn't exist !");
+        else{
+            System.out.println("Slang word");
+            for(String i : slanglist)
+                System.out.println(i);
         }
     }
 }
